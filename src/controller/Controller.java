3b92e97db037b8fe6202e299fd5d355b443c0d74
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-03-31 21:19:26 +0200

    Create Controller.java

---
+++ src/controller/Controller.java
@@ -0,0 +1,75 @@
+package controller;
+
+//import javax.swing.*
+import javax.swing.JList;
+
+import java.awt.*;
+import java.util.*;
+
+import model.ContactDAO;
+import model.ContactModel;
+import view.View;
+
+public class Controller
+{
+	
+	
+	private ContactModel modele;
+	private View vue;
+	private ContactDAO contactDao;
+	private ArrayList<ContactModel> arrayContacts = null;
+	
+	
+	public Controller(ContactModel modele, View vue)
+	{
+		this.modele = modele;
+		this.vue = vue;
+		this.contactDao = new ContactDAO();
+		initController();
+	}
+	
+	
+	
+	public Controller(View view)
+	{
+		
+		this.vue = view;
+		contactDao = new ContactDAO();
+		arrayContacts = contactDao.getAllContacts();
+		vue.getList().setListData(arrayContacts.toArray());
+		
+		
+	}
+	
+	
+	public void initController()
+	{
+		vue.getBtnAjouter().addActionListener(ActionEvent -> ajouterContact());
+		
+	}
+	
+
+	private void ajouterContact()
+	{
+		String nom = vue.getTextField().getText();
+		String prenom = vue.getTextField_1().getText();
+		String mail = vue.getTextField_2().getText();
+		String tel = vue.getTextField_3().getText();
+		
+		
+		modele = new ContactModel();
+		modele.setNom_cont(nom);
+		modele.setPrenom_cont(prenom);
+		modele.setMail_cont(mail);
+		modele.setTel_cont(tel);
+		
+		contactDao.ajouterContact(modele);
+		
+		arrayContacts = contactDao.getAllContacts();
+		vue.getList().setListData(arrayContacts.toArray());
+		
+		vue.getLblNewLabel().setText("contact ajouté");
+	}
+	
+
+}
