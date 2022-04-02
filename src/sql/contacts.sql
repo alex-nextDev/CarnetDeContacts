f2849767629293a7ac8a3ca57a987ce5b5c391fc
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-04-02 11:15:21 +0200

    Update contacts.sql

--- src/sql/contacts.sql
+++ src/sql/contacts.sql
@@ -3,4 +3,5 @@ CREATE TABLE contacts(id_cont int(6) NOT NULL AUTO_INCREMENT,
                       prenom_cont varchar(250) NOT NULL,
                       mail_cont varchar(250) NOT NULL,
                       tel_cont varchar(250) NOT NULL,
-                      CONSTRAINT pk_contact PRIMARY KEY (id_cont));
+                      CONSTRAINT pk_contact PRIMARY KEY (id_cont)
+                     );
