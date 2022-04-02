5e49ba78e872ad933aa2609ef7d3fd19f1ab5d41
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-04-02 11:14:40 +0200

    Create contacts.sql

---
+++ src/sql/contacts.sql
@@ -0,0 +1,6 @@
+CREATE TABLE contacts(id_cont int(6) NOT NULL AUTO_INCREMENT,
+                      nom_cont varchar(250) NOT NULL,
+                      prenom_cont varchar(250) NOT NULL,
+                      mail_cont varchar(250) NOT NULL,
+                      tel_cont varchar(250) NOT NULL,
+                      CONSTRAINT pk_contact PRIMARY KEY (id_cont));
