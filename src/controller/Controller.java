d03c0205b03e7100e87dfd9b1f1d432be772cbf1
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-04-05 21:52:50 +0200

    Update Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,7 +1,9 @@
 package controller;
 
-import javax.swing.JList;
+import javax.swing.*;
 import java.util.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import model.ContactDAO;
 import model.ContactModel;
 import view.View;
@@ -24,15 +26,15 @@ public class Controller
 		initController();
 	}
 	
-	
-	
-	public Controller(View view)
+
+	public Controller(View vue)
 	{
 		
-		this.vue = view;
+		this.vue = vue;
 		contactDao = new ContactDAO();
 		arrayContacts = contactDao.getAllContacts();
 		vue.getList().setListData(arrayContacts.toArray());
+		initController();
 		
 		
 	}
@@ -42,9 +44,16 @@ public class Controller
 	{
 		vue.getBtnAjouter().addActionListener(ActionEvent -> ajouterContact());
 		
+		vue.getList().addListSelectionListener(ListSelectionEvent -> selectionnerContact());
+		vue.getBtnUpdate().addActionListener(ActionEvent -> modifierContact());
+		vue.getBtnDelete().addActionListener(ActionEvent -> supprimerContact());
+		//vue.getBtnErase().addActionListener(ActionEvent -> effacerContact());
+		
+		
 	}
 	
 
+	
 	private void ajouterContact()
 	{
 		String nom = vue.getTextField().getText();
@@ -52,6 +61,7 @@ public class Controller
 		String mail = vue.getTextField_2().getText();
 		String tel = vue.getTextField_3().getText();
 		
+		//Reconstruction du contact à partir de la BDD
 		
 		modele = new ContactModel();
 		modele.setNom_cont(nom);
@@ -67,5 +77,75 @@ public class Controller
 		vue.getLblNewLabel().setText("contact ajouté");
 	}
 	
+	
+	private void selectionnerContact()
+	{
+		int index = vue.getList().getSelectedIndex();
+		if(index == -1)
+			return;
+		
+		ContactModel modele = (ContactModel) arrayContacts.get(index);
+		
+		vue.getIdLabel().setText(modele.getId_cont() + "");
+		vue.getTextField().setText(modele.getNom_cont());
+		vue.getTextField_1().setText(modele.getPrenom_cont());
+		vue.getTextField_2().setText(modele.getMail_cont());
+		vue.getTextField_3().setText(modele.getTel_cont());
+	}
 
+	
+	private void modifierContact()
+	{
+		String idCont = vue.getIdLabel().getText();
+		int id__ = Integer.parseInt(idCont);
+		String nom = vue.getTextField().getText();
+		String prenom = vue.getTextField_1().getText();
+		String mail = vue.getTextField_2().getText();
+		String tel = vue.getTextField_3().getText();
+		
+		
+		modele = new ContactModel();
+		modele.setId_cont(id__);
+		modele.setNom_cont(nom);
+		modele.setPrenom_cont(prenom);
+		modele.setMail_cont(mail);
+		modele.setTel_cont(tel);
+		
+		contactDao.modifierContact(modele);
+		
+		arrayContacts = contactDao.getAllContacts();
+		vue.getList().setListData(arrayContacts.toArray());
+		
+		vue.getLblNewLabel().setText("contact modifié");
+	}
+	
+	private void supprimerContact()
+	{
+		
+		String idCont = vue.getIdLabel().getText();
+		int id__ = Integer.parseInt(idCont);
+		String nom = vue.getTextField().getText();
+		String prenom = vue.getTextField_1().getText();
+		String mail = vue.getTextField_2().getText();
+		String tel = vue.getTextField_3().getText();
+
+		
+	
+		modele = new ContactModel();
+		modele.setId_cont(id__);
+		modele.setNom_cont(nom);
+		modele.setPrenom_cont(prenom);
+		modele.setMail_cont(mail);
+		modele.setTel_cont(tel);
+	
+		
+		contactDao.supprimerContact(modele);
+		
+		arrayContacts = contactDao.getAllContacts();
+		vue.getList().setListData(arrayContacts.toArray());
+		
+		vue.getLblNewLabel().setText("contact supprimé");
+		
+	}
+	
 }
